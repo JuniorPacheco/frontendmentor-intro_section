18078b9dec04af3082e4eb7fcda4deff0b43cf30
JuniorPacheco <junior.pacheco.356@gmail.com>
2023-02-10 17:47:12 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App text-[18px]">
+      <h1>Hola mundo!</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "almost-white": "hsl(0, 0%, 98%)",
+        "medium-gray": "hsl(0, 0%, 41%)",
+        "almost-black": "hsl(0, 0%, 8%)",
+      },
+    },
+    fontFamily: {
+      epiloge: ["Epilogue", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
